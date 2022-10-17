3b7241261e77daa1b3b5e5521b251209471fe10a
CalebUleth <caleb.woelcke@uleth.ca>
2022-10-17 11:36:32 -0600

    This is a message

--- www/js/scripts.js
+++ www/js/scripts.js
@@ -5,3 +5,24 @@ var app = new Framework7({
 });
 var mainView = app.views.create('.view-main')
 
+var waterLevel;
+var fertizelevel;
+var health;
+
+function water(){//adds water
+  waterLevel+=10;
+}
+
+function fertilize(){//adds fertlizer
+  fertizelevel+=10;
+}
+
+function trim(){
+  health+=10;
+}
+
+function process(){
+  if(waterLevel<50 ||waterlevel > 70){
+    health-=20
+  }
+}
