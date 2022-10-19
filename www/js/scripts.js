67990974d141555d451f1690e53c9ab3da73a566
CalebUleth <caleb.woelcke@uleth.ca>
2022-10-19 11:50:04 -0600

    Adds code

--- www/js/scripts.js
+++ www/js/scripts.js
@@ -5,24 +5,46 @@ var app = new Framework7({
 });
 var mainView = app.views.create('.view-main')
 
-var waterLevel;
-var fertizelevel;
-var health;
+var waterLevel=50;
+var fertizeLevel=50;
+var health=50;
+var nearDeath=false;
+
+var timeout
 
 function water(){//adds water
-  waterLevel+=10;
+  waterLevel= Math.min(waterLevel+25);
 }
 
 function fertilize(){//adds fertlizer
-  fertizelevel+=10;
+  fertizeLevel = Math.min(fertilizeLevel+25, 100);
 }
 
 function trim(){
-  health+=10;
+  health= Math.min();
 }
 
 function process(){
-  if(waterLevel<50 ||waterlevel > 70){
-    health-=20
+  waterLevel = Math.max(0, waterLevel-5);
+  fertizeLevel = Math.max(0, fertizeLevel-5);
+
+  
+  if(waterLevel<25 ||waterLevel > 75){
+    health= Math.max(health-5,0);
   }
+  else if(fertizeLevel<25|| fertizeLevel>75){
+    health= Math.max(health-5,0);
+  }
+  
+  console.log("Health:"+health);
+  if (health < 40){
+    if(nearDeath == false){
+      console.log("dead");
+      nearDeath = true;
+      $("#plant path").css("fill", "chocolate");
+    }
+  }
+
+  var waterTimer = setTimeout(process, 1000)
 }
+process();
